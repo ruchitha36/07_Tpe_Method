3cb1a6cc27d2201bfd77a90fe71ffca469da667d
ruchitha36 <122854121+ruchitha36@users.noreply.github.com>
2023-01-21 19:36:19 +0530

    Add files via upload

---
+++ Type_Method.py
@@ -0,0 +1,32 @@
+""" 
+Day:Thursday
+Date: 19th Jan 2023
+Topic: Type Method
+Description: In python type method is used to return the data type of the value passed in the argument.
+"""
+
+
+''' One way of using Type Method'''
+
+# print(type(4))
+# print(type(76.42))
+# print(type(-34))
+# print(type('hi'))
+# print(type(True))
+# print(type(False))
+
+''' Second way of using Type Method'''
+
+# a = " Welcome to Type Method"
+# b = '43'
+# c = -746
+# d = ('Hello World')
+# e = 10.23
+# f = 11.2
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+# print(type(f))
